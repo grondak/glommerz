694ab915ac49bd0776e7142824e6d5ac2d7c0b7b
Tony Reynolds <grondak@gmail.com>
2014-08-23 19:42:34 -0400

    Angularizing the app, with four fake events in JSON. Externalized Styles.

---
+++ scripts/glommerz/app.js
@@ -0,0 +1,28 @@
+﻿var MyCtrl = function ($scope) {
+    $scope.items = [
+        {
+            placeName: "Tennesse Rodeo Association",
+            date: "1408835135000",
+            eventName: "We loves us a Rodeo",
+            hint: "Say you like Tennesee Walking Horses"
+        },
+        {
+            placeName: "Loyal Order of Water Buffalo",
+            date: "1408835135000",
+            eventName: "Recruiting event",
+            hint: "Yabba Dabba Doo!"
+        },
+        {
+            placeName: "WT&S",
+            date: "1408831000000",
+            eventName: "Job fair",
+            hint: "Blue Light Specials"
+        },
+        {
+            placeName: "HackerSpace212",
+            date: "1408830000000",
+            eventName: "Electronics Hackathon",
+            hint: "Tell them you just love that 555 timer."
+        }
+    ]
+}
